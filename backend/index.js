c138661d950401f061eb085c6bc6eb2d346ff937
Carlos Sosa <isek7or@gmail.com>
2024-02-13 22:38:15 -0800

    backend updated

--- backend/index.js
+++ backend/index.js
@@ -5,14 +5,14 @@ import cors from 'cors';
 
 const app = express();
 
-app.use(bodyParser.json({ limit: "30mb", extended: true}));
-app.use(bodyParser.urlencoded({ limit: "30mb", extended: true}));
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
 const CONNECTION_URL = 'mongodb+srv://mil3ston3proj3ct2:Mp87654321.@cluster0.lfrb59x.mongodb.net/?retryWrites=true&w=majority'
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, userUnifiedTopology: true})
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, userUnifiedTopology: true })
     .then(() => app.listen(PORT, () => console.log('Server running on port: ${PORT')))
     .catch((error) => console.log(error.message));
 
