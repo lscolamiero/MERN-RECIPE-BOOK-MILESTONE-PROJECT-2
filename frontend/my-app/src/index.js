32051fcc962d30ce3bfbdb83398b6c3d29efb69b
Jackury13 <143567012+Jackury13@users.noreply.github.com>
2024-02-14 22:48:53 -0800

    add recipe card photos and info

--- frontend/my-app/src/index.js
+++ frontend/my-app/src/index.js
@@ -1,3 +1,7 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
