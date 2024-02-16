1383114d0bc276b542845b6192bc3d7c797e7bc2
Jackury13 <143567012+Jackury13@users.noreply.github.com>
2024-02-15 20:00:45 -0800

    move gitnore into front end

--- backend/index.js
+++ backend/index.js
@@ -1,17 +1,17 @@
-const express = require("express");
-const app = express();
-const cors = require("cors");
-const dotenv = require("dotenv");
-dotenv.config();
-const router = express.Router();
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
 
-app.use(express.json());
+const app = express();
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
-module.exports = router;
+const CONNECTION_URL = 'mongodb+srv://mil3ston3proj3ct2:Mp87654321.@cluster0.lfrb59x.mongodb.net/?retryWrites=true&w=majority'
+const PORT = process.env.PORT || 5050;
 
-if (config) {
-  app.listen(process.env.PORT, () => {
-    console.log(`Server Started on port ${process.env.PORT}`);
-  });
-}
+mongoose.connect(CONNECTION_URL)
+    .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+    .catch((error) => console.log(error.message));
