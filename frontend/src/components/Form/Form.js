b2ec25635569e55e72cf8f2f519595d96159ef1e
Carlos Sosa <isek7or@gmail.com>
2024-02-15 19:43:40 -0800

    created card on FE

--- frontend/src/components/Form/Form.js
+++ frontend/src/components/Form/Form.js
@@ -27,8 +27,8 @@ const Form = () => {
                 <Typography variant="h6">Create your Recipe</Typography>
                 <TextField name="creator" variant="outlined" label="Creator" fullWidth value={postData.creator} onChange={(e) => setPostData({ ...postData, creator: e.target.value })} />
                 <TextField name="title" variant="outlined" label="Title" fullWidth value={postData.title} onChange={(e) => setPostData({ ...postData, title: e.target.value })} />
-                <TextField name="ingredients" variant="outlined" label="Ingredients" fullWidth multiline minRows={4} value={postData.ingredients} onChange={(e) => setPostData({ ...postData, ingredients: e.target.value })} />
-                <TextField name="instructions" variant="outlined" label="Instructions" fullWidth multiline minRows={4} value={postData.instructions} onChange={(e) => setPostData({ ...postData, instructions: e.target.value })} />
+                <TextField name="ingredients" variant="outlined" label="Ingredients" fullWidth multiline minRows={3} value={postData.ingredients} onChange={(e) => setPostData({ ...postData, ingredients: e.target.value })} />
+                <TextField name="instructions" variant="outlined" label="Instructions" fullWidth multiline minRows={3} value={postData.instructions} onChange={(e) => setPostData({ ...postData, instructions: e.target.value })} />
                 <TextField name="tags" variant="outlined" label="Tags (coma separated)" fullWidth value={postData.tags} onChange={(e) => setPostData({ ...postData, tags: e.target.value.split(',') })} />
                 <div className={classes.fileInput}><FileBase type="file" multiple={false} onDone={({ base64 }) => setPostData({ ...postData, selectedFile: base64 })} /></div>
                 <Button className={classes.buttonSubmit} variant="contained" color="primary" size="large" type="submit" fullWidth>Submit</Button>
