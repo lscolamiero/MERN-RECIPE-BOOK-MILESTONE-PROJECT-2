599623433e869162a12e2c00798fde2dfd33e2c9
Carlos Sosa <isek7or@gmail.com>
2024-02-17 00:31:01 -0800

    added delete and like routes/functions

--- frontend/src/components/Posts/Post/Post.js
+++ frontend/src/components/Posts/Post/Post.js
@@ -5,9 +5,14 @@ import DeleteIcon from '@material-ui/icons/Delete';
 import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
 import moment from 'moment';
 import useStyles from './styles';
+import { useDispatch } from 'react-redux';
+
+import { deletePost, likePost } from '../../../actions/posts';
 
 const Post = ({ post, setCurrentId }) => {
     const classes = useStyles();
+    const dispatch = useDispatch();
+
     return (
         <Card className={classes.card}>
             <CardMedia className={classes.media} image={post.selectedFile} title={post.title} />
@@ -16,11 +21,7 @@ const Post = ({ post, setCurrentId }) => {
                 <Typography variant='body2'>{moment(post.createdAt).fromNow()}</Typography>
             </div>
             <div className={classes.overlay2}>
-                <Button
-                    style={{ color: 'white' }}
-                    size='small'
-                    onClick={() => setCurrentId(post._id)}>
-                    <MoreHorizIcon frontSize='default' />
+                <Button style={{ color: 'white' }} size='small' onClick={() => setCurrentId(post._id)}><MoreHorizIcon fontSize='medium' />
                 </Button>
             </div>
             <div className={classes.details}>
@@ -34,15 +35,8 @@ const Post = ({ post, setCurrentId }) => {
                 <Typography className={classes.title} vatiant='h5' gutterBottom>{post.instructions}</Typography>
             </CardContent>
             <CardActions className={classes.cardActions}>
-                <Button size='small' color='primary' onClick={() => { }}>
-                    <ThumbUpAltIcon fontSize='small' />
-                    Like
-                    {post.likeCount}
-                </Button>
-                <Button size='small' color='primary' onClick={() => { }}>
-                    <DeleteIcon fontSize='small' />
-                    Delete
-                </Button>
+                <Button size='small' color='primary' onClick={() => dispatch(likePost(post._id))}><ThumbUpAltIcon fontSize='small' /> Like {post.likeCount} </Button>
+                <Button size='small' color='primary' onClick={() => dispatch(deletePost(post._id))}><DeleteIcon fontSize='small' /> Delete </Button>
             </CardActions>
         </Card>
 
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { configureStore, applyMiddleware, compose } from '@reduxjs/toolkit';
 import { thunk } from 'redux-thunk';
@@ -9,11 +9,11 @@ import reducers from './reducers';
 import App from './App';
 import './index.css';
 
-const store = configureStore({reducer:reducers}, compose(applyMiddleware(thunk)))
+const store = configureStore({ reducer: reducers }, compose(applyMiddleware(thunk)))
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
     <Provider store={store}>
         <App />
-    </Provider>,
-    document.getElementById('root')
+    </Provider>
 );
